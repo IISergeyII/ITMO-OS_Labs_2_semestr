6ac4b4e43b1da975068c060ff042934a49ce180e
IISergeyII <31198738+IISergeyII@users.noreply.github.com>
2020-09-20 13:28:44 +0300

    Update 1_01.sh

--- OSLab1/Lab_1/1_01.sh
+++ OSLab1/Lab_1/1_01.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+a=$1
+b=$2
+c=$3
+
+if [[ ( "$a" -gt "$b" ) && ( "$a" -gt "$c" ) ]]
+then echo "$a is the greatest"
+elif [[ ( "$b" -gt "$a" ) && ( "$b" -gt "$c" ) ]]
+then echo "$b is the greatest"
+else echo "$c is the greatest"
+fi
