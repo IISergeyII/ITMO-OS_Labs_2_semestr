81d15e822113b168dd2dee37e78928e2f645677d
IISergeyII <31198738+IISergeyII@users.noreply.github.com>
2020-10-05 11:48:12 +0300

    Update 2_06.sh

--- Lab2/2_06.sh
+++ Lab2/2_06.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 grep "VmRSS" /proc/*/status | head -n -2 | sed "s/[^0-9.]\+/ /g" | sed "s/ //"| 
-sort -nk2 | head -1 | awk '{print "PID: "$1" VmRSS: "$2}'
+sort -nk2 | tail -n1 | awk '{print "PID: "$1" VmRSS: "$2}'
    
